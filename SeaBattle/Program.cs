649048cb1b566892ecebc1fff044a5160d10fcee
ashurakova <shurakovaelena@bk.ru>
2020-02-09 14:04:28 +0300

    Demo 1.0

--- SeaBattle/Program.cs
+++ SeaBattle/Program.cs
@@ -8,7 +8,23 @@ namespace SeaBattle
         static void Main(string[] args)
         {
             Console.Title = "Морской бой";
-            
+            var User = new User();
+            var Bot = new Bot();
+            Boolean yes = true;
+            while (yes)
+            {
+                while (true)
+                {
+                    User.Output(User.BattleField);
+                    User.Strike();
+                    if (User.Win())
+                    {
+                        break;
+                    }
+                }
+                Console.SetCursorPosition(30, 1);
+                Console.WriteLine("Спасибо за игру!");
+            }
         }
     }
 }
