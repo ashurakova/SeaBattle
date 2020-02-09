e4ce7eacc47d358f3795a3b7d5e620fd4fbab1d3
ashurakova <shurakovaelena@bk.ru>
2020-02-09 13:52:13 +0300

    Program.cs commit

---
+++ SeaBattle/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SeaBattleLibrary;
+
+namespace SeaBattle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Морской бой";
+            
+        }
+    }
+}
