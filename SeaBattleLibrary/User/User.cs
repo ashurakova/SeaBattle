59913a3dbf6f50cb81809ec0a06ca111f8737e69
ashurakova <shurakovaelena@bk.ru>
2020-02-16 22:06:12 +0300

    User class refactoring and adding UserStepValidation class

--- SeaBattleLibrary/User/User.cs
+++ SeaBattleLibrary/User/User.cs
@@ -10,110 +10,64 @@ namespace SeaBattleLibrary
         public void StepMaking()
         {
             Console.SetCursorPosition(30, BattleShip.Indent++);
-            Console.WriteLine("Выстрел №: " + (Step+1));
-            bool LetterValid = false;
-            while (LetterValid != true)
+            Console.WriteLine("Выстрел №: " + (Step + 1));
+            Console.SetCursorPosition(30, BattleShip.Indent++);
+            Console.Write("Ваш выстрел:");
+            string InputtedValue = Console.ReadLine();
+            bool ValidStepMade = false;
+            while (ValidStepMade==false)
             {
-                Console.SetCursorPosition(30, BattleShip.Indent++);
-                Console.Write("Ваш выстрел:");
-                switch (Console.Read())
+               
+                int InputtedLetter;
+                int InputtedNumber;
+
+                bool InputValidity = UserStepValidation.StepValidation(InputtedValue, out InputtedLetter, out InputtedNumber);
+                if (InputValidity == true)
                 {
-                        case 'а':
-                            Letter[Step] = 0;
-                            LetterValid = true;
-                            break;
-                        case 'б':
-                            Letter[Step] = 1;
-                            LetterValid = true;
-                            break;
-                        case 'в':
-                            Letter[Step] = 2;
-                            LetterValid = true;
-                            break;
-                        case 'г':
-                            Letter[Step] = 3;
-                            LetterValid = true;
-                            break;
-                        case 'д':
-                            Letter[Step] = 4;
-                            LetterValid = true;
-                            break;
-                        case 'е':
-                            Letter[Step] = 5;
-                            LetterValid = true;
-                            break;
-                        case 'ж':
-                            Letter[Step] = 6;
-                            LetterValid = true;
-                            break;
-                        case 'з':
-                            Letter[Step] = 7;
-                            LetterValid = true;
-                            break;
-                        case 'и':
-                            Letter[Step] = 8;
-                            LetterValid = true;
-                            break;
-                        case 'к':
-                            Letter[Step] = 9;
-                            LetterValid = true;
-                            break;
-                        default:
-                            Console.SetCursorPosition(30, BattleShip.Indent++);
-                            Console.WriteLine("Пожалуйста, введите строчную букву от а до до к и цифру от 1 до 10 в формате 'a1'.");
-                            break;
-                    }
-                if (LetterValid == true)
-                {
-                    try
+                    Console.SetCursorPosition(30, 0);
+                    Console.WriteLine("HELLO");
+                    Letter[Step] = InputtedLetter;
+                    Index[Step] = InputtedNumber;
+                    string ConsoleClear = "                            ";
+                    Console.SetCursorPosition(30, 0);
+                    Console.WriteLine(ConsoleClear);
+                    if (BattleShip.BotField[Index[Step], Letter[Step]] == 0)
                     {
-                        Index[Step] = Convert.ToInt32(Console.ReadLine()) - 1;
-                        if (Index[Step] >= 0 && Index[Step] < 10)
-                        {
-                            string ConsoleClear = "                            ";
-                            Console.SetCursorPosition(30, 0);
-                            Console.WriteLine(ConsoleClear);
-                            if (BattleShip.BotField[Index[Step], Letter[Step]] == 0)
-                            {
-                                BattleShip.BotField[Index[Step], Letter[Step]] = 3;
-                                BattleShip.BattleField[Index[Step], Letter[Step]] = 3;
-                                BattleShip.Output(BattleShip.BattleField);
-                                Console.SetCursorPosition(30, 0);
-                                Console.Write("Промах!");
-                                Step++;
-                            }
-                            else if (BattleShip.BotField[Index[Step], Letter[Step]] == 1)
-                            {
-                                BattleShip.BotField[Index[Step], Letter[Step]] = 2;
-                                BattleShip.BattleField[Index[Step], Letter[Step]] = 2;
-                                BattleShip.Output(BattleShip.BattleField);
-                                BattleShip.Points++;
-                                Console.SetCursorPosition(30, 0);
-                                Console.Write("Попадание!");
-                                Step++;
-                            }
-                            else
-                            {
-                                Console.SetCursorPosition(30, 0);
-                                Console.Write("Нельзя стрелять в эту клетку");
-                                Console.SetCursorPosition(30, 4);
-                                Console.WriteLine();
-                            }
-                        }
-                        else
-                        {
-                            Console.SetCursorPosition(30, BattleShip.Indent++);
-                            Console.WriteLine("Пожалуйста, введите строчную букву от а до до к и цифру от 1 до 10 в формате 'a1'.");
-                        }
+                        BattleShip.BotField[Index[Step], Letter[Step]] = 3;
+                        BattleShip.BattleField[Index[Step], Letter[Step]] = 3;
+                        BattleShip.Output(BattleShip.BattleField);
+                        Console.SetCursorPosition(30, 0);
+                        Console.Write("Промах!");
+                        Step++;
+                    }
+                    else if (BattleShip.BotField[Index[Step], Letter[Step]] == 1)
+                    {
+                        BattleShip.BotField[Index[Step], Letter[Step]] = 2;
+                        BattleShip.BattleField[Index[Step], Letter[Step]] = 2;
+                        BattleShip.Output(BattleShip.BattleField);
+                        BattleShip.Points++;
+                        Console.SetCursorPosition(30, 0);
+                        Console.Write("Попадание!");
+                        Step++;
+                    }
+                    else
+                    {
+                        Console.SetCursorPosition(30, 0);
+                        Console.Write("Нельзя стрелять в эту клетку");
+                        Console.SetCursorPosition(30, 4);
+                        Console.WriteLine();
 
                     }
-                    catch
-                    {
-                        Console.SetCursorPosition(30, BattleShip.Indent++);
-                        Console.WriteLine("Пожалуйста, введите строчную букву от а до до к и цифру от 1 до 10 в формате 'a1'.");
-                    }
+                    ValidStepMade = true;
                 }
-            } 
+                else
+                {
+                    Console.SetCursorPosition(30, BattleShip.Indent++);
+                    Console.WriteLine("Пожалуйста, введите строчную букву от а до до к и цифру от 1 до 10 в формате 'a1'.");
+                    ValidStepMade = false;
+                    break; 
+                }
+            }
         }
     }
 }
---
+++ SeaBattleLibrary/User/UserStepValidation.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace SeaBattleLibrary
+{
+    public class UserStepValidation
+    {
+        public static bool StepValidation(string z, out int ValidLetter, out int ValidNumber)
+        { 
+            char InputtedLetter = z[0];
+            int CheckedLetter;
+            LetterValidation(InputtedLetter, out CheckedLetter);
+            int CheckedNumber;
+            try
+            {
+                int InputtedNumber = Int32.Parse(z.Substring(1));
+                NumberValidation(InputtedNumber, out InputtedNumber);
+                CheckedNumber = InputtedNumber;
+            }
+            catch
+            {
+                CheckedNumber = -1;
+            }
+
+            if (CheckedLetter != -1 && CheckedNumber != -1)
+            {
+                ValidLetter = CheckedLetter;
+                ValidNumber = CheckedNumber;
+                return true;
+            }
+            else
+            {
+                ValidLetter = -1;
+                ValidNumber = -1;
+                return false;
+            }
+        }
+
+        private static void LetterValidation(char z, out int CheckedLetter)
+        {
+            int Letter=-1;
+            switch (z)
+            {
+                case 'а':
+                    Letter = 0;
+                break;
+                case 'б':
+                    Letter = 1;
+                break;
+                case 'в':
+                    Letter = 2;
+                break;
+                case 'г':
+                    Letter = 3;
+                break;
+                case 'д':
+                    Letter = 4;
+                break;
+                case 'е':
+                    Letter = 5;
+                break;
+                case 'ж':
+                    Letter = 6;
+                break;
+                case 'з':
+                    Letter = 7;
+                break;
+                case 'и':
+                    Letter = 8;
+                break;
+                case 'к':
+                    Letter = 9;
+                break;    
+            }
+            if (Letter >= 0 && Letter < 10)
+            {
+                CheckedLetter = Letter;
+            }
+            else
+            {
+                CheckedLetter = Letter;
+            }
+        }
+        private static void NumberValidation (int z, out int CheckedNumber)
+        {
+            if (z>0&&z<11)
+            {
+                CheckedNumber = z - 1;
+            }
+            else
+            {
+                CheckedNumber = -1;
+            }
+                
+        }
+    }
+}
